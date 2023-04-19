b25b16bbcca9186fc61003ff6f454c4b0c04e866
mikellol <miguel.velardecbtis@gmail.com>
2023-04-19 00:24:56 -0600

    first commit

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,15 @@
+import { AppPage } from './app.po';
+
+describe('new App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should be blank', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toContain('The world is your oyster.');
+  });
+});
+
---
+++ src/app/services/tweet.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TweetService {
+  constructor(private http: HttpClient) { }
+  onSearch(hashtag: string, count: number ) {
+    const url = `https://af1993.ir/api/twitter/${hashtag}/${count}`;
+    const options = {
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer $2y$10$1irbofmTl7bAuII1O3jaZu4ykkIQAQxFvnnGQ9kHDlxM19lifPxe2'
+      })
+    }
+    
+    return this.http.get(url, options);
+  }
+}
